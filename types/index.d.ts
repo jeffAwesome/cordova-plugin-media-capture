71ee998ea00bc89ac28721153fc83fb755c5b1d6
Jeff Richardson <jeff@masterpieceinteractive.com>
2017-04-18 23:49:52 -0500

    add type to video options

--- types/index.d.ts
+++ types/index.d.ts
@@ -154,6 +154,8 @@ interface VideoOptions {
     limit?: number;
     /** The maximum duration of a video clip, in seconds. */
     duration?: number;
+
+    quality?: boolean;
 }
 
 /** Encapsulates a set of media capture parameters that a device supports. */
